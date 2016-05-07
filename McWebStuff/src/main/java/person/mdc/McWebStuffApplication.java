193d53b30296464a008fcde67aec45878f0791ab
mcass <mcass@10.0.0.101>
2016-05-06 20:25:59 -0600

    added db to web

--- McWebStuff/src/main/java/person/mdc/McWebStuffApplication.java
+++ McWebStuff/src/main/java/person/mdc/McWebStuffApplication.java
@@ -1,10 +1,21 @@
 package person.mdc;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @SpringBootApplication
 public class McWebStuffApplication {
+	@Bean
+	@Primary
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().username("dbUser").password("dbPass").url("jdbc:derby:mcwebstuff_db;create=true")
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(McWebStuffApplication.class, args);
