b174b9b4acd69a1da90e564f318367bb4ebe97de
mcass <mcass@10.0.0.101>
2016-05-06 20:21:57 -0600

    web work

--- McWebStuff/src/main/resources/static/app/app.component.ts
+++ McWebStuff/src/main/resources/static/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  selector: 'McWebStuff',
+  templateUrl: 'app/app.html'
 })
-export class AppComponent { }
+export class AppComponent {
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
+}
 
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
