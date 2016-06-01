4ccbbbb55976a796e09a431dfcd5c8f8ee9ecf88
Michael Cassidy <michael.cassidy@ldschurch.org>
2016-06-01 14:41:07 -0600

    More minor cleanup

--- McStuff/McStuff_web/src/test/java/mcstuff/web/McWebStuffApplicationTests.java
+++ McStuff/McStuff_web/src/test/java/mcstuff/web/McWebStuffApplicationTests.java
@@ -2,15 +2,22 @@ package mcstuff.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class McWebStuffApplicationTests {
+	
+	@Autowired 
+	private ApplicationContext applicationContext;
 
 	@Test
 	public void contextLoads() {
+		Assert.notNull(applicationContext);
 	}
 
 }
