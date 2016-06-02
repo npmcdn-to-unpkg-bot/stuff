a6830ba6549fcd280d56eecc2e37bd4d63ec3a3a
Michael Cassidy <michael.cassidy@ldschurch.org>
2016-06-02 09:25:13 -0600

    Minor cleanup

--- McStuff/McStuff_web/src/main/resources/static/app/app.js
+++ McStuff/McStuff_web/src/main/resources/static/app/app.js
@@ -1,25 +1,32 @@
 
-function AppComponent($rootRouter) {
-	$rootRouter.navigate(['Home']);
-}
+(function() {
 
-angular.module('app', [
-	'ngComponentRouter',
-	'home'
-])
+	'use strict';
 
-.config(function($locationProvider) {
-  $locationProvider.html5Mode(true);
-})
+	angular.module('app', [
+		'ngComponentRouter',
+		'home'
+	])
+	
+	.config(function($locationProvider) {
+		$locationProvider.html5Mode(true);
+	})
+	
+	.value('$routerRootComponent', 'app')
+	
+	.component('app', {
+		templateUrl: '/app/app.html',
+		controller: AppComponent,
+		$routeConfig: [
+			{path: '/home', name: 'Home', component: 'homeComponent'}
+		]
+	});
+	
+	function AppComponent($rootRouter) {
+		var $comp = this;
+	
+		$rootRouter.navigate(['Home']);
+	}
 
-.value('$routerRootComponent', 'app')
-
-.component('app', {
-  templateUrl: '/app/app.html',
-  controller: AppComponent,
-  $routeConfig: [
-    {path: '/home', name: 'Home', component: 'homeComponent'}
-  ]
-});
-
+})();
 
--- McStuff/McStuff_web/src/main/resources/static/app/home.js
+++ McStuff/McStuff_web/src/main/resources/static/app/home.js
@@ -1,13 +1,19 @@
 
-function HomeComponent($scope, $element, $attrs) {
-	var $comp = this;
+(function() {
+
+	'use strict';
+
+	angular.module('home',[])
 	
-}
+	.component('homeComponent', {
+		templateUrl: '/app/home.html',
+		controller: HomeComponent,
+		bindings: { $router: '<' }
+	});
+		
+	function HomeComponent($location, $scope, $element, $attrs) {
+		var $comp = this;
+		debugger;
+	}
 
-angular.module('home',[])
-
-.component('homeComponent', {
-  templateUrl: '/app/home.html',
-  controller: HomeComponent,
-  bindings: { $router: '<' }
-})
+})();
