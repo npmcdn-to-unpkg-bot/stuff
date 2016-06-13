68d31ebfe59ba96ab140fa166e70d1d90377ef75
Michael Cassidy <michael.cassidy@ldschurch.org>
2016-06-13 10:07:31 -0600

    Copyleft work

--- McStuff/McStuff_app/src/main/java/mcstuff/startup/StartupSceneController.java
+++ McStuff/McStuff_app/src/main/java/mcstuff/startup/StartupSceneController.java
@@ -1,3 +1,19 @@
+/* Copyright (C) 2016+ Michael Cassidy - All Rights Reserved
+	This file is a part of McStuff
+
+    McStuff is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    McStuff is distributed as sample code in the hope that it will be 
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with McStuff.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 package mcstuff.startup;
 
 import java.net.URL;
@@ -12,7 +28,6 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 
@@ -36,7 +51,7 @@ public class StartupSceneController implements Initializable {
 		final Set<I_Module> modules = appConfig.getModules();
 		for (final I_Module module : modules) {
 			final Button btnModule = new Button();
-			btnModule.getStyleClass().add("btn_record-sales");
+			btnModule.getStyleClass().add("btnModule");
 			btnModule.setText(module.getTitle());
 			btnModule.onActionProperty().set(new EventHandler<ActionEvent>() {
 				@Override
